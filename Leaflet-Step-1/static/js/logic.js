49eb991d27377ea1ac728818012c40c0cdc982f1
Margot <margoterudy@gmail.com>
2022-01-20 13:46:09 -0600

    Pop UP describing the place and time each earthquake with circle markers

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,7 +1,7 @@
 // Define earthquakes plates GeoJSON url variable
 var earthquakesURLoneday = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/1.0_day.geojson";
 
-// Create earthquake layerGroup
+// Create "earthquakes" layerGroup
 var earthquakes = L.layerGroup();
 
 // Create tile layer
@@ -23,7 +23,7 @@ var myMap = L.map("mapid", {
   layers: [grayscaleMap, earthquakes]
 });
 
-d3.json(earthquakesURL, function(earthquakeData) {
+d3.json(earthquakesURLoneday, function(earthquakeData) {
 
 
   // Determine the earthquake marker size by their magnitude
@@ -41,10 +41,41 @@ d3.json(earthquakesURL, function(earthquakeData) {
       case depth > 50:
         return "orange";
       case depth > 30:
-        return "yellow";
+        return "lightorange";
       case depth > 10:
-        return "lightyellow";
+        return "yellow";
       default:
         return "green";
     }
   }
+
+  
+  // Create a GeoJSON layer 
+  // Pop UP describing the place and time each earthquake with circle markers
+  L.geoJSON(earthquakeData, {
+    pointToLayer: function (feature, latlng) {
+      return L.circleMarker(latlng, 
+
+
+        // Set style of the markers 
+        {
+          radius: markerSize(feature.properties.mag),
+          fillColor: chooseColor(feature.geometry.coordinates[2]),
+          fillOpacity: 0.5,
+          color: "darkgreen",
+          stroke: true,
+          weight: 0.5
+        }
+      );
+    },
+    onEachFeature: function(feature, layer) {
+      layer.bindPopup("<h3>Location: " + feature.properties.place + "</h3><hr><p>Date: "
+      + new Date(feature.properties.time) + "</p><hr><p>Magnitude: " + feature.properties.mag + "</p>");
+    }
+  }).addTo(earthquakes);
+
+   // Sending  earthquakes layer to the createMap function
+   earthquakes.addTo(myMap);
+
+});
+
